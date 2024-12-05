e73a76e1e2bde47e29d90509c49ed3870456dd13
Tarun Akash <124859424+Tarunakash04@users.noreply.github.com>
2024-12-05 15:24:36 +0530

    Update LanguageTranslation.py
    Changed from LibreTranslate to MyMemory
    Works perfectly fine

--- LanguageTranslation.py
+++ LanguageTranslation.py
@@ -1,36 +1,39 @@
 from pypdf import PdfReader as pdf
 import requests as req
 
-reader = pdf(r'C:\Users\Tarun Akash\Desktop\ML-Ops.pdf')
+reader = pdf(r'C:\Users\Tarun Akash\Desktop\ML-Ops1.pdf')
 page = reader.pages[0]
 texted = page.extract_text()
-print(texted)
 
 file_path = "newtext.txt"
 with open(file_path, 'w') as file:
     file.write(texted)
 
 def langtrans(text, source_lang, target_lang):
-    url = 'https://libretranslate.com/translate'
-
-    data = {
-        'q': text,                
-        'source': source_lang,    
-        'target': target_lang,    
-        'format': 'text'          
+    url = "https://api.mymemory.translated.net/get"
+    
+    params = {
+        "q": text,
+        "langpair": f"{source_lang}|{target_lang}"
     }
-
-    response = req.post(url, data=data)
-
+    
+    response = req.get(url, params=params)
+    
     if response.status_code == 200:
-        translated_text = response.json()['translatedText'] 
+        translated_text = response.json()['responseData']['translatedText']
         return translated_text
     else:
-        print("Error:", response.status_code) 
+        print(f"Error: {response.status_code}")
         return None
 
-tt = langtrans(texted, 'en', 'de')
-if tt:
-    print(tt)
+text = texted
+translated = langtrans(text, "en", "de")
+if translated:
+    print(f"Translated text: {translated}")
 else:
-    print("failed")
+    print("Failed to translate.")
+
+
+file_path = "newtranslatedtext.txt"
+with open(file_path, 'w') as file:
+    file.write(translated)
